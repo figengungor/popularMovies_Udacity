64798f859dc711b2cac6ea450292ac490f5e6817
figengungor <figengungor1@gmail.com>
2018-02-19 17:33:06 +0300

    Add movie list and detail UI

---
+++ app/src/main/java/com/figengungor/popularmovies/Constants.java
@@ -0,0 +1,13 @@
+package com.figengungor.popularmovies;
+
+/**
+ * Created by figengungor on 2/19/2018.
+ */
+
+public class Constants {
+    private static final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+    private static final String POSTER_PATH = "w185/";
+    private static final String BACKDROP_PATH = "w300/";
+    private static final String POSTER_URL = IMAGE_BASE_URL + POSTER_PATH;
+    private static final String BACKDROP_URL = IMAGE_BASE_URL + BACKDROP_PATH;
+}
